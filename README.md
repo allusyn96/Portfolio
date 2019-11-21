4d9cdd9ee43d7dbe8dc881500d1eb00935d7e9c9
Alison Miguel <alisonrawr@Alisons-MBP.attlocal.net>
2019-11-21 10:05:00 -0800

    Added Twitter Streamer for streaming tweets, Twitter Client to examine tweets based on a specific user, and Twitter Listener to print out the tweets to stdout

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio
-Projects posted on my portfolio website
---
+++ Twitter/twitter_access.py
@@ -0,0 +1,6 @@
+#Variables that contain the user credentials to access Twitter API.
+
+ACCESS_TOKEN = '420856215-HfFI8Vu9BO7R05m1zpQmcv1WyCUGSep2oMa8Vdu6'
+ACCESS_TOKEN_SECRET = 'UIY4uZXnu7nqNfqRVeWVMFICzB3o7u7ivRTZn0ArZdGtM'
+CONSUMER_KEY = 'HIgTTQPTstth5n2GgoSdIgVrF'
+CONSUMER_SECRET = '7pEZWmMwXv8TGdIa1Ta0E7H2cnxBdUTeG3uGQc8BNbQfHoiYPp'
